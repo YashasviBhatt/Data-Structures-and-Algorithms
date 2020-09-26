2174403afaa7f3ea78a8963a45cb049cb5970af0
Yashasvi Bhatt <yashasvibhatt9@gmail.com>
2020-09-26 09:17:56 +0530

    Stack

---
+++ stack.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<conio.h>
+#define max 10
+
+void push(int);
+void pop();
+void peek();
+void diplay();
+
+int stack[max];
+int top = -1;
+
+typedef enum{
+	t, f
+}boolean;
+
+void main(){
+	int choice, item;
+	boolean not_exit = t;
+	
+	while (not_exit == t){
+		printf("\n\nEnter\n");
+		printf("1. To Push\n");
+		printf("2. To Pop\n");
+		printf("3. To Peek\n");
+		printf("4. To Display Stack\n");
+		printf("0. To Exit\n");
+		printf("Enter Your choice : ");
+		scanf("%d", &choice);
+	
+		switch(choice){
+			case 0 : not_exit = f; break;
+			case 1 : {
+				printf("\nEnter Item to Push : ");
+				scanf("%d", &item);
+				push(item);
+			} break;
+			case 2 : pop(); break;
+			case 3 : peek(); break;
+			case 4 : display(); break;
+			default : printf("\nWrong Choice, Please Try Again");
+		}
+	}
+}
+
+// function to push value in stack
+void push(int item){
+	if (top == (max - 1))
+		printf("Stack Overflow");
+	else
+		stack[++top] = item;
+}
+
+// function to pop value from stack
+void pop(){
+	if (top == -1)
+		printf("Stack Underflow");
+	else
+		printf("Popped Item : %d", stack[top--]);
+}
+
+// function to peek the top most item
+void peek(){
+	if (top == -1)
+		printf("Stack Underflow");
+	else
+		printf("Top-Most Item : %d", stack[top]);
+}
+
+// function to display stack
+void display(){
+	int index;
+	printf("Stack : ");
+	if (top == -1)
+		printf("Stack Underflow");
+	else{
+		for (index = 0; index <= top; index++)
+			printf("%d ", stack[index]);
+	}
+}
