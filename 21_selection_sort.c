5622ee8daf82dec61e3164324b4b4cb088a58914
Yashasvi Bhatt <yashasvibhatt9@gmail.com>
2020-11-15 16:51:53 +0530

    Added Selection_Sort Implementation

---
+++ 21_selection_sort.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+// Function to Sort Array using Selection Sort Algorithm
+void selection_sort(int arr[], int n)
+{
+    int index1, index2, temp;
+    for(index1 = 0 ; index1 < n - 1 ; index1++)
+        for(index2 = index1 + 1 ; index2 < n ; index2++)
+            if(arr[index1] > arr[index2])
+            {
+                temp = arr[index1];
+                arr[index1] = arr[index2];
+                arr[index2] = temp;
+            }
+}
+
+int main()
+{
+    int arr[10];
+    int n, index;
+
+    printf("Enter Number of Elements : ");
+    scanf("%d", &n);
+
+    if(n > 10 || n < -1)
+    {
+        printf("Invalid Input");
+        return 0;
+    }
+
+    printf("Enter Elements\n");
+    for(index = 0 ; index < n ; index++)
+    {
+        printf("Element %d : ", index + 1);
+        scanf("%d", &arr[index]);
+    }
+
+    selection_sort(arr, n);
+
+    printf("\nSorted Array\n");
+    for(index = 0 ; index < n ; index++)
+    {
+        printf("Element %d : %d\n", index + 1, arr[index]);
+    }
+    return 0;
+}
