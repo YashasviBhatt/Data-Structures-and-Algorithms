937527efcd78e2e1a2a9e96ea08b146ac4fc0b18
Yashasvi Bhatt <yashasvibhatt9@gmail.com>
2020-11-15 17:16:35 +0530

    Added Shell_Sort Implementation

---
+++ 22_shell_sort.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+// Function to Sort Array using Insertion Sort Algorithm
+void shell_sort(int arr[], int n)
+{
+    int index1, temp, gap = n / 2;
+    while(gap > 0)
+    {
+        for(index1 = 0 ; index1 < (n - gap) ; index1++)
+            if(arr[index1] > arr[index1 + gap])
+            {
+                temp = arr[index1];
+                arr[index1] = arr[index1 + gap];
+                arr[index1 + gap] = temp;
+            }
+        gap /= 2;
+    }
+}
+
+int main()
+{
+    int arr[10];
+    int n, index;
+
+    printf("Enter Number of Elements : ");
+    scanf("%d", &n);
+
+    if(n > 10 || n < -1)
+    {
+        printf("Invalid Input");
+        return 0;
+    }
+
+    printf("Enter Elements\n");
+    for(index = 0 ; index < n ; index++)
+    {
+        printf("Element %d : ", index + 1);
+        scanf("%d", &arr[index]);
+    }
+
+    shell_sort(arr, n);
+
+    printf("\nSorted Array\n");
+    for(index = 0 ; index < n ; index++)
+    {
+        printf("Element %d : %d\n", index + 1, arr[index]);
+    }
+    return 0;
+}
