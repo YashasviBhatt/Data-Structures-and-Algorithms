1891ae84fee9cc2e5a6626032b88e16aee800cd5
Yashasvi Bhatt <yashasvibhatt9@gmail.com>
2020-11-15 16:22:50 +0530

    Added Bubble_Sort Implementation

---
+++ 19_bubble_sort.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+int bubble_sort(int arr[], int n)
+{
+    int index1, index2, temp, flag = 0;
+    for(index1 = 0 ; index1 < n - 1 ; index1++)
+    {
+        for(index2 = 0 ; index2 < n - index1 - 1 ; index2++)
+            if(arr[index2] > arr[index2 + 1])
+            {
+                temp = arr[index2];
+                arr[index2] = arr[index2 + 1];
+                arr[index2 + 1] = temp;
+                flag = 1;
+            }
+        if(flag != 1)
+            break;
+        else
+            flag = 0;
+    }
+}
+
+int main()
+{
+    int arr[10];
+    int n, index;
+
+    printf("Enter Number of Elements : ");
+    scanf("%d", &n);
+
+    if(n > 10 || n < -1)
+    {
+        printf("Invalid Input");
+        exit(0);
+    }
+
+    printf("Enter Elements\n");
+    for(index = 0 ; index < n ; index++)
+    {
+        printf("Element %d : ", index + 1);
+        scanf("%d", &arr[index]);
+    }
+
+    bubble_sort(arr, n);
+
+    printf("\nSorted Array\n");
+    for(index = 0 ; index < n ; index++)
+    {
+        printf("Element %d : %d\n", index + 1, arr[index]);
+    }
+}
